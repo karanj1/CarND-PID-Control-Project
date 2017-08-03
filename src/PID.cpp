b62282a7dd2e5c8f90a99f689480edc00fb8376b
karanj1 <karanj54321@gmail.com>
2017-08-03 14:55:27 -0700

    t2p4 02 main.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  Kp = Kp;
+  Ki = Ki;
+  Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+ // d_error is difference from old cte (p_error) to the new cte
+  d_error = (cte - p_error);
+  // p_error gets set to the new cte
+  p_error = cte;
+  // i_error is the sum of ctes to this point
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
